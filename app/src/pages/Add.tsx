041675ae9aadf926fb5688622b19db00fa2e985b
Ram-Ram007 <ramajayam2411@gmail.com>
2023-10-28 23:58:16 +0530

    modified

--- app/src/pages/Add.tsx
+++ app/src/pages/Add.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import MovieForm from "../components/MovieForm";
 
 function Add() {
--- app/src/pages/Edit.tsx
+++ app/src/pages/Edit.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import MovieForm from "../components/MovieForm";
 
 function Edit() {
