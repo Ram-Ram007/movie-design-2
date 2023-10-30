7b5bda11603e329cc4031007645602652e2c8cb2
Ram-Ram007 <ramajayam2411@gmail.com>
2023-10-30 23:40:14 +0530

    changed

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -18,21 +18,15 @@ const Home: React.FC<IHome> = ({ handleEdit }) => {
   const [isMovieDeleted, setIsMovieDeleted] = useState(false);
   const [deleteError, setDeleteError] = useState<string | null>(null);
 
-  // Initialize loading state for each movie
-  const [movieLoadingStates, setMovieLoadingStates] = useState<boolean[]>([]);
-
   useEffect(() => {
     async function getMoviesFromAPI() {
       setIsLoading(true);
       try {
         const response = await getMovies();
         setMovies(response.data);
-        
-        setMovieLoadingStates(new Array(response.data.length).fill(false));
       } catch (error) {
         console.error("Error fetching movies:", error);
-        setDeleteError("Network error");
-        setIsMovieDeleted(true);
+        setDeleteError("Network Error");
       } finally {
         setIsLoading(false);
       }
@@ -41,22 +35,14 @@ const Home: React.FC<IHome> = ({ handleEdit }) => {
     getMoviesFromAPI();
   }, [refresh]);
 
-  async function handleDeleteMovie(id: number, index: number) {
-    // Create a copy of the loading state array and set loading for the specific movie
-    const updatedLoadingStates = [...movieLoadingStates];
-    updatedLoadingStates[index] = true;
-    setMovieLoadingStates(updatedLoadingStates);
-
+  async function handleDeleteMovie(id: number) {
     try {
       await deleteMovie(id);
       setIsMovieDeleted(true);
       setRefresh((prev) => !prev);
     } catch (error) {
-      console.error("Error deleting movie:", error);
       setDeleteError("Error deleting the movie.");
     } finally {
-      updatedLoadingStates[index] = false;
-      setMovieLoadingStates(updatedLoadingStates);
     }
   }
 
@@ -69,36 +55,39 @@ const Home: React.FC<IHome> = ({ handleEdit }) => {
     <>
       <Layout title="home">
         <h1>Home</h1>
-
         <div className="container">
           <Link to="/add" role="button" className="secondary">
             +
           </Link>
+
           <button
             disabled={isLoading}
             onClick={() => setRefresh((prev) => !prev)}
           >
             {isLoading ? <LoadingIcon /> : <>refresh list</>}
           </button>
-          <div className="grid">
-            {movies.map((m, index) => (
-              <article key={m.id}>
-                <h1>{m.title}</h1>
-                <h1>{m.year}</h1>
+          {isLoading ? (
+            <p>Loading movies!</p>
+          ) : (
+            <div className="grid">
+              {movies.map((m) => (
+                <article key={m.id}>
+                  <h1>{m.title}</h1>
+                  <h1>{m.year}</h1>
 
-                <Link to={`/edit/${m.id}`} className="pico-link">
-                  <button onClick={() => handleEdit(m)}>Edit</button>
-                </Link>
-                <button
-                  disabled={movieLoadingStates[index]}
-                  onClick={() => handleDeleteMovie(m.id, index)}
-                  className="pico-link"
-                >
-                  {movieLoadingStates[index] ? <LoadingIcon /> : <>Delete</>}
-                </button>
-              </article>
-            ))}
-          </div>
+                  <Link to={`/edit/${m.id}`} className="pico-link">
+                    <button onClick={() => handleEdit(m)}>✏️</button>
+                  </Link>
+                  <button
+                    onClick={() => handleDeleteMovie(m.id)}
+                    className="pico-link"
+                  >
+                    🚮
+                  </button>
+                </article>
+              ))}
+            </div>
+          )}
         </div>
       </Layout>
 
